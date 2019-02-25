2765db08c85790cd1a9f4db3dcbda9baed582a62
Kenn <kenn+git@code-crew.org>
2019-02-25 08:40:41 -0600

    update

---
+++ accountApp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Account(models.Model):
+    name = models.CharField(default="", max_length=200)
+    checking = models.DecimalField(max_digits=16, decimal_places=4)
+    savings = models.DecimalField(max_digits=16, decimal_places=4)
+    dateTimeOpen = models.DateTimeField(default=timezone.now)
